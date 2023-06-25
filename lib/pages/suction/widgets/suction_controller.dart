17f3d67fde5ea6fae036cb06a7df3bc551042362
duke <19833038@qq.com>
2023-06-25 15:18:14 +0800

    remake

--- lib/pages/suction/widgets/suction_controller.dart
+++ lib/pages/suction/widgets/suction_controller.dart
@@ -14,18 +14,43 @@ class SuctionController extends StatefulWidget {
 class _SuctionControllerState extends State<SuctionController> {
   bool _isStarted = false;
   double _percent = 0;
+  int _totalTime = 0;
   int _seconds = 0;
   int _milliseconds = 0;
+  late Timer _timer;
   Duration duration = const Duration(milliseconds: 10);
 
   void _onStart() {
     if (_isStarted) return;
-
-    Timer.periodic(duration, (timer) {});
-
-    setState(() {
-      _isStarted = true;
+    _isStarted = true;
+    _timer = Timer.periodic(duration, (timer) {
+      if (_totalTime >= 1500) {
+        timer.cancel();
+        return;
+      } else {
+        setState(() {
+          _totalTime++;
+          _seconds = _totalTime ~/ 100;
+          _milliseconds = _totalTime % 100;
+          _percent = _totalTime / 1500;
+        });
+      }
     });
+  }
+
+  void _onReset() {
+    setState(() {
+      _timer.cancel();
+      _isStarted = false;
+      _percent = 0;
+      _totalTime = 0;
+      _seconds = 0;
+      _milliseconds = 0;
+    });
+  }
+
+  String _formatTime(int time) {
+    return time < 10 ? '0$time' : '$time';
   }
 
   @override
@@ -33,22 +58,26 @@ class _SuctionControllerState extends State<SuctionController> {
     return Container(
       padding: const EdgeInsets.all(32),
       child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-        AppButton(text: 'Reset', onPressed: () {}),
+        AppButton(text: 'Reset', onPressed: _onReset),
         CircularPercentIndicator(
-            percent: 0,
+            percent: _percent,
             lineWidth: 8,
             radius: 60,
             circularStrokeCap: CircularStrokeCap.round,
             progressColor: Colors.blue,
             backgroundColor: Colors.grey[200]!,
-            center: TextButton(
-              onPressed: () {},
-              style: TextButton.styleFrom(
-                padding: const EdgeInsets.all(38),
-                shape: const CircleBorder(),
-              ),
-              child: const Text('Start', style: TextStyle(fontSize: 20)),
-            )),
+            center: _isStarted
+                ? Text(
+                    '${_formatTime(_seconds)} : ${_formatTime(_milliseconds)}',
+                    style: TextStyle(fontSize: 20))
+                : TextButton(
+                    onPressed: _onStart,
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(38),
+                      shape: const CircleBorder(),
+                    ),
+                    child: const Text('Start', style: TextStyle(fontSize: 20)),
+                  )),
         AppButton(text: 'Update', onPressed: () {}),
       ]),
     );
